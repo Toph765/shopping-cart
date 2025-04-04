f7ea9e9c332833e63725bb55ea6442a6fdee9781
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-04-04 19:09:54 +0800

    feat: fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { Outlet, Link } from 'react-router-dom'
 
 function App() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const src = await response.json();
+        setProducts(src);
+      }
+      catch (error) {
+        console.log(error);
+      }
+
+    }
+
+    getProducts();
+  }, [])
+
   return (
     <>
       <nav>
         <Link to="homepage">Home</Link>
         <Link to="shoppage">Shop</Link>
+        <Link to="cart">Cart</Link>
       </nav>
-      <Outlet />
+      <Outlet context={{products}}/>
     </>
   )
 }
