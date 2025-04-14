bf2a9218ff5b0ecb655917be3f07f8ba112e3669
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-04-14 16:41:11 +0800

    feat: diplay cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,15 +2,29 @@ import { useOutletContext } from "react-router-dom";
 import ItemCard from "./ItemCard";
 
 const Cart = () => {
-    const { cart, setCart } = useOutletContext();
+    const { cart, handleSetCart } = useOutletContext();
+
+    const displayTotal = () => {
+        const list = [];
+
+        cart.forEach(item => {
+            const itemPrice = item.count * item.price;
+            list.push(itemPrice);
+        });
+
+        const total = list.reduce((accumulator, value) => accumulator + value, 0);
+
+        return <p>Total: {total}</p>;
+    }
 
     return (
         <div>
             {cart && (cart.map((obj) => {
                 return (
-                    <ItemCard key={obj.id} obj={obj} cart={cart} setCart={setCart} />
+                    <ItemCard key={obj.id} obj={obj} cart={cart} onChange={handleSetCart} />
                 );
-                }))}
+            }))}
+            {cart && displayTotal()}
         </div>
     )
 };
