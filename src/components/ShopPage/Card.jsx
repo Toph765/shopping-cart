c653db778106d89bba7462bc88faa5e8a4d59aaa
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-04-14 16:41:45 +0800

    feat: add <dialog>

--- src/components/ShopPage/Card.jsx
+++ src/components/ShopPage/Card.jsx
@@ -1,8 +1,27 @@
 import styles from "./ShopPage.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Card = ({ obj, addToCart }) => {
     const [number, setNumber] = useState(1);
+
+    const dialogRef = useRef(null);
+    const testRef = useRef(null);
+
+    const openModal = () => {
+        dialogRef.current.showModal();
+    }
+
+    const closeModal = () => {
+        dialogRef.current.close();
+    }
+
+    const openTest = () => {
+        testRef.current.showModal();
+    }
+
+    const closeTest = () => {
+        testRef.current.close();
+    }
 
     const product = {
         "id": obj.id,
@@ -29,26 +48,44 @@ const Card = ({ obj, addToCart }) => {
     const handleAddToCartBtn = (e) => {
         e.preventDefault();
         product["count"] = number;
-        console.log("from cart", product.count);
         addToCart(product);
         setNumber(1);
+        closeModal();
+        openTest();
     }
 
     return (
         <div>
-            <img src={obj.image} alt={obj.category} className={styles.img} />
-            <p>{obj.title}</p>
-            <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
-            <p>{obj.description}</p>
-            <p>{"$" + obj.price}</p>
-            <form>
+            <dialog ref={dialogRef}>
                 <div>
-                    <button onClick={decNumber}>-</button>
-                    <input type="number" value={number} min="1" onChange={(e) => handleInputChange(e)}/>
-                    <button onClick={incNumber}>+</button>
+                    <img src={obj.image} alt={obj.category} className={styles.img} />
+                    <p>{obj.title}</p>
+                    <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
+                    <p>{obj.description}</p>
+                    <p>{"$" + obj.price}</p>
+                    <form>
+                        <div>
+                            <button onClick={decNumber}>-</button>
+                            <input type="number" value={number} min="1" onChange={(e) => handleInputChange(e)}/>
+                            <button onClick={incNumber}>+</button>
+                        </div>
+                    <button onClick={e => handleAddToCartBtn(e)}>Add to Cart</button>
+                    </form>
                 </div>
-            <button onClick={e => handleAddToCartBtn(e)}>Add to Cart</button>
-            </form>
+                <button onClick={closeModal}>close</button>
+            </dialog>
+
+            <dialog ref={testRef}>
+                <p>Added to cart!</p>
+                <button onClick={closeTest}>okay</button>
+            </dialog>
+            
+            <div onClick={openModal}>
+                <img src={obj.image} alt={obj.category} className={styles.img} />
+                <p>{obj.title}</p>
+                <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
+                <p>{"$" + obj.price}</p>
+            </div>
         </div>
     )
 };
