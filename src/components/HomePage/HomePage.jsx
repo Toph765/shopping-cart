1b6cfb22b3606e8142d76a5d2faf55f430a063ad
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-04-24 20:18:24 +0800

    fix: move file

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.header}>E t - c e t e r a</h2>
+            <p className={styles.text}>Random stuff you can buy!</p>
+        </div>
+    )
+};
+
+export default HomePage
