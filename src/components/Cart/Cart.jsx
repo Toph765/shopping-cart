bba828abe2b850479f3e8ac32e9f25bdd27c0c49
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-04-24 17:59:50 +0800

    feat: add checkout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,20 @@
-import { useOutletContext } from "react-router-dom";
+import { useRef } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import ItemCard from "./ItemCard";
 import EmptyCart from "./EmptyCart";
 
 const Cart = () => {
     const { cart, handleSetCart } = useOutletContext();
+    const reference = useRef(null);
+    const homeNav = useNavigate();
+    const shopNav = useNavigate();
+
+    const handleCheckOutBtn = (e) => {
+        e.preventDefault();
+
+        handleSetCart([]);
+        reference.current.showModal();
+    }
 
     const displayTotal = () => {
         const list = [];
@@ -15,11 +26,35 @@ const Cart = () => {
 
         const total = list.reduce((accumulator, value) => accumulator + value, 0);
 
-        return <p>Total: {total}</p>;
+        return (
+            <div>
+                <p>Total: {total}</p>
+                <button onClick={e => handleCheckOutBtn(e)}>Check Out</button>
+            </div>
+        );
+    }
+
+    const handleHomeBtn = (e) => {
+        e.preventDefault();
+
+        homeNav('/homepage');
+        reference.current.close();
+    }
+
+    const handleShopBtn = (e) => {
+        e.preventDefault();
+
+        shopNav('/shoppage');
+        reference.current.close();
     }
 
     return (
         <div>
+            <dialog ref={reference}>
+                <p>Thank you for Shopping!</p>
+                <button onClick={(e) => handleHomeBtn(e)}>Home</button>
+                <button onClick={(e) => handleShopBtn(e)}>Shop</button>
+            </dialog>
             {cart.length === 0 ? (
                 <EmptyCart/>
             ) : (
