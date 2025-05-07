9cec1c4f600969832792a6f83dd777dbf706bc1a
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-05-07 20:07:03 +0800

    feat: add classnames; reconfigure tags

--- src/components/ShopPage/Card.jsx
+++ src/components/ShopPage/Card.jsx
@@ -56,35 +56,43 @@ const Card = ({ obj, addToCart }) => {
 
     return (
         <div>
-            <dialog ref={dialogRef}>
-                <div>
-                    <img src={obj.image} alt={obj.category} className={styles.img} />
-                    <p>{obj.title}</p>
-                    <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
-                    <p>{obj.description}</p>
-                    <p>{"$" + obj.price}</p>
-                    <form>
+            <dialog ref={dialogRef} className={styles.dialog}>
+                <div className={styles.cartDialog}>
+                    <img src={obj.image} alt={obj.category} className={styles.dialogImg} />
+                    <div className={styles.dialogText}>
                         <div>
-                            <button onClick={decNumber}>-</button>
-                            <input type="number" value={number} min="1" onChange={(e) => handleInputChange(e)}/>
-                            <button onClick={incNumber}>+</button>
+                            <button onClick={closeModal} className={styles.close}>X</button>
+                            <p className={styles.dialogTitle}>{obj.title}</p>
+                            <p>{"$" + obj.price}</p>
+                            <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
+                            <p className={styles.description}>{obj.description}</p>
                         </div>
-                    <button onClick={e => handleAddToCartBtn(e)}>Add to Cart</button>
-                    </form>
+                        <form>
+                            <button onClick={decNumber} className={styles.inputBtn}>-</button>
+                            <input className={styles.input} type="number" value={number} min="1" onChange={(e) => handleInputChange(e)}/>
+                            <button onClick={incNumber} className={styles.inputBtn}>+</button>
+                            <button className={styles.addBtn} onClick={e => handleAddToCartBtn(e)}>Add to Cart</button>
+                        </form>
+                    </div>
                 </div>
-                <button onClick={closeModal}>close</button>
             </dialog>
 
-            <dialog ref={testRef}>
-                <p>Added to cart!</p>
-                <button onClick={closeTest}>okay</button>
+            <dialog ref={testRef} className={styles.message}>
+                <div className={styles.msgCont}>
+                    <p>Added to cart!</p>
+                    <button className={styles.okay} onClick={closeTest}>okay</button>
+                </div>
             </dialog>
             
-            <div onClick={openModal}>
-                <img src={obj.image} alt={obj.category} className={styles.img} />
-                <p>{obj.title}</p>
-                <p>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
-                <p>{"$" + obj.price}</p>
+            <div className={styles.card} onClick={openModal}>
+                <div className={styles.firstCont}>
+                    <img src={obj.image} alt={obj.category} className={styles.img} />
+                    <p className={styles.title}>{obj.title}</p>
+                </div>
+                <div className={styles.secondCont}>
+                    <p className={styles.rating}>rating: {obj.rating.rate + "/5"} {`(${obj.rating.count})`}</p>
+                    <p className={styles.price}>{"$" + obj.price}</p>
+                </div>
             </div>
         </div>
     )
