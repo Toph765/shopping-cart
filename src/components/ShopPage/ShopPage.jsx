d9595d700d8bcc26c0efe9c3d3e01460babcd5e8
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2025-05-07 20:05:55 +0800

    feat: add loading and error

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -3,20 +3,35 @@ import Card from "./Card";
 import styles from "./ShopPage.module.css";
 
 const ShopPage = () => {
-    const { products, addToCart } = useOutletContext();
+    const { products, addToCart, loading, error } = useOutletContext();
 
     return (
-        <div className={styles.container}>
-            {products && (products.map((obj) => {
-                return (
-                    <Card
-                        key={obj.id}
-                        obj={obj}
-                        addToCart={addToCart}
-                    />
-                )
-            }))}
+        <div>
+            {loading && (
+                <div className={styles.loading}>
+                    <p>Loading...</p>
+                </div>
+            )}
+
+            {error && (
+                <div className={styles.error}>
+                    <p>{'Something went wrong :('}</p>
+                    <p>{error}</p>
+                </div>
+            )}
+            <div className={styles.container}>
+                {products && (products.map((obj) => {
+                    return (
+                        <Card
+                            key={obj.id}
+                            obj={obj}
+                            addToCart={addToCart}
+                        />
+                    )
+                }))}
+            </div>
         </div>
+        
     )
 };
 
